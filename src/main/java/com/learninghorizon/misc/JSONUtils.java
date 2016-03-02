c6c7e3b83c44f4cd653970df4677004dc727a074
BHARAT SAVANI <savanibharat@gmail.com>
2016-03-01 19:49:30 -0800

    JSONUtils

---
+++ src/main/java/com/learninghorizon/misc/JSONUtils.java
@@ -0,0 +1,30 @@
+package com.learninghorizon.misc;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public final class JSONUtils {
+
+	private JSONUtils(){
+		throw new AssertionError(
+			"Don't try to access Private constructor."
+		);
+	}
+	
+	public static 
+	String convertMapToString(final Map<String, Object> map){
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+		try {
+			return objectMapper.writerWithDefaultPrettyPrinter()
+							   .writeValueAsString(map);
+		} catch (final JsonProcessingException processingException) {
+			processingException.printStackTrace();
+		}
+		return null;
+	}
+}
